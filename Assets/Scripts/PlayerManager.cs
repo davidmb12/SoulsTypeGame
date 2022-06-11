30bea1586f99efc4758395ae348df138545bd379
David Murillo <david.murillo@cetys.edu.mx>
2022-06-11 18:32:27 -0500

    Init commit

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DM
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+        Animator anim;
+        CameraHandler cameraHandler;
+        PlayerLocomotion playerLocomotion;
+        public bool isInteracting;
+
+        [Header("PlayerFlags")]
+        public bool isSprinting;
+        public bool isInAir;
+        public bool isGrounded;
+
+
+
+        private void Awake()
+        {
+            cameraHandler = CameraHandler.singleton;
+        }
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+        }
+
+        void Update()
+        {
+            float delta = Time.deltaTime;
+            isInteracting = anim.GetBool("isInteracting");
+            
+
+            inputHandler.TickInput(delta);
+            playerLocomotion.HandleMovement(delta);
+            playerLocomotion.HandleRollingAndSprinting(delta);
+            //playerLocomotion.HandleFalling(delta, playerLocomotion.moveDirection);
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+
+            if (cameraHandler != null)
+            {
+                Debug.Log("entro");
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, inputHandler.mouseX, inputHandler.mouseY);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+            isSprinting = inputHandler.b_input;
+
+            if (isInAir)
+            {
+                playerLocomotion.inAirTimer = playerLocomotion.inAirTimer + Time.deltaTime;
+            }
+        }
+    }
+}
+
